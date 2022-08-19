cbfbd4bcdf597d75214822dca3359e682e6a55f7
Brunna Lage <brunnalage@gmail.com>
2022-08-19 18:19:17 -0300

    Feat: adding switch case to Title styles

--- imc-challenge/src/Components/Title/Title.js
+++ imc-challenge/src/Components/Title/Title.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { TitleStyled } from "./Title.styled";
+import { TitleH2, TitleH3, TitleH4, TitleH5 } from "./Title.styled";
 
-const Title = ({Title}) => { 
-    return(
-    <TitleStyled>{Title}</TitleStyled>
-    )
+const Title = ({tagName, Title}) => { 
+    switch (tagName) {
+        case "h2":
+            <TitleH2>{Title}</TitleH2>
+        case "h3":
+            return <TitleH3>{text}</TitleH3>
+        case "h4":
+            return <TitleH4>{text}</TitleH4>
+        case "h5":
+            return <TitleH5>{text}</TitleH5>      
+    
+};
 };
 
 export default Title;
