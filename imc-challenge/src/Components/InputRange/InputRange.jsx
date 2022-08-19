d2d94782f6fbaaa0ef5155f758405928f027a8dd
Brunna Lage <brunnalage@gmail.com>
2022-08-19 19:42:44 -0300

    Feat: create functional Input Range component

---
+++ imc-challenge/src/Components/InputRange/InputRange.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { InputRangeStyled } from "./InputRange.styled"
+
+const InputRange = () => {
+    return(
+        <InputRangeStyled type="range"/>
+    )
+}
+
+export default InputRange;
