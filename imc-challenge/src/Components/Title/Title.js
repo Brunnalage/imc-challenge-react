fdb75601986a7fbd59c075b781ae4ccbc775a0d9
Brunna Lage <brunnalage@gmail.com>
2022-08-19 22:22:38 -0300

    Refactor: changing from switch case to id and className props

--- imc-challenge/src/Components/Title/Title.js
+++ imc-challenge/src/Components/Title/Title.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { TitleH2, TitleH3, TitleH4, TitleH5 } from "./Title.styled";
+import { TitleStyled } from "./Title.styled";
 
-const Title = ({tagName, Title}) => { 
-    switch (tagName) {
-        case "h2":
-            <TitleH2>{Title}</TitleH2>
-        case "h3":
-            return <TitleH3>{Title}</TitleH3>
-        case "h4":
-            return <TitleH4>{Title}</TitleH4>
-        case "h5":
-            return <TitleH5>{Title}</TitleH5>      
-    
+const Title = ({id, Title, Info}) => {     
+    return(
+    <TitleStyled className={`${id}`}id={id}>{Title}{Info}</TitleStyled>
+    )    
 };
-};
+
 
 export default Title;
